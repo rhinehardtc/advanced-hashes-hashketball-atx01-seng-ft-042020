ce813c3593d9530c81615d8b8c5cd242b4b98dfc
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-07 03:35:13 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,8 +227,8 @@ def big_shoe_rebounds
   game_hash.each do |location, team|
     team[:players].each do |player|
       if player[:shoe] > biggest_shoe
-        biggest_shoe == player[:shoe]
-        rebounds == player[:rebounds]
+        biggest_shoe = player[:shoe]
+        rebounds = player[:rebounds]
       end
     end
   end
